2359100c8ae69893d478ee7a1fc76337bffa063f
BillyChristian! <107543274+onekt@users.noreply.github.com>
2026-05-28 08:16:37 +0200

    main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel 
 
 app=FastAPI()
+class UserQuestion(BaseModel):
+    question:str
+    
 @app.get("/")
 def home():
     return{"message": "RAG API is running"}
+
+@app.post("/ask")
+def ask_question(heAsked: UserQuestion):
+    return{
+        "question_asked": heAsked.question
+    }
