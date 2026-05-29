9658d19fd447e5a2f88e5456fd12294f39199249
BillyChristian! <107543274+onekt@users.noreply.github.com>
2026-05-29 15:07:26 +0200

    adding chunks to model

--- app/ingest.py
+++ app/ingest.py
@@ -2,12 +2,27 @@ from pypdf import PdfReader
 
 def load_pdf(file_path):
     reader = PdfReader(file_path)
-    text=""
-    
+    text = ""
+
     for page in reader.pages:
         text += page.extract_text()
-        return text
+
+    return text
     
-if __name__=="__main__":
+def chunk_text(text, chunk_size=500):
+    chunks = []
+
+    for i in range(0, len(text), chunk_size):
+        chunk = text[i:i + chunk_size]
+        chunks.append(chunk)
+
+    return chunks
+
+if __name__ == "__main__":
     text = load_pdf("documents/doc.pdf")
-    print (text)
+
+    chunks = chunk_text(text)
+
+    print(chunks[0])
+    print("TOTAL CHUNKS:", len(chunks))
+        
